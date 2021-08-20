6ec6abe79f506b54df73b5e92bead778442e633d
Gurshan Kaile <gurshankaile206@gmail.com>
2021-08-20 16:21:43 -0700

    add animals controller

--- AnimalShelter/Controllers/AnimalsControllers.cs
+++ AnimalShelter/Controllers/AnimalsControllers.cs
@@ -36,15 +36,64 @@ namespace AnimalShelter.Controllers
       return animal;
     }
 
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Animal animal)
+    {
+      if (id != animal.AnimalId)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(animal).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!AnimalExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+        // POST: api/Animals
     [HttpPost]
     public async Task<ActionResult<Animal>> Post(Animal animal)
     {
       _db.Animals.Add(animal);
       await _db.SaveChangesAsync();
 
-      
       return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId }, animal);
+    }
 
+    // DELETE: api/Animals/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAnimal(int id)
+    {
+      var animal = await _db.Animals.FindAsync(id);
+      if (animal == null)
+      {
+        return NotFound();
+      }
+
+      _db.Animals.Remove(animal);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+
+    private bool AnimalExists(int id)
+    {
+      return _db.Animals.Any(e => e.AnimalId == id);
     }
   }
 }
