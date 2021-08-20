c88e9cfcefbc39c83937bf5dab0a727c1ca3c76b
Gurshan Kaile <gurshankaile206@gmail.com>
2021-08-20 15:33:36 -0700

    add custom API endpoints

---
+++ AnimalShelter/Controllers/AnimalsControllers.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AnimalShelter.Models;
+
+namespace AnimalShelter.Controllers
+{
+  [Route("apiu/[controller")]
+  [ApiController]
+  public class AnimalsController : ControllerBase
+  {
+    private readonly AnimalShelterContext _db;
+
+    public AnimalsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Animal>>> Get()
+    {
+      return await _db.Animals.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Animal>> GetAnimal(int id)
+    {
+      var animal = await _db.Animals.FindAsync(id);
+
+      if (animal == null)
+      {
+        return NotFound();
+      }
+
+      return animal;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Animal>> Post(Animal animal)
+    {
+      _db.Animals.Add(animal);
+      await _db.SaveChangesAsync();
+
+      
+      return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId }, animal);
+
+    }
+  }
+}
